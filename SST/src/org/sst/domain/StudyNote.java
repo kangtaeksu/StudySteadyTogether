f6c29bb71731c0e2506dc9f0cce1c9ea77d52681
gangtaeksoo@naver.com <gangtaeksoo@naver.com>
2021-04-08 14:47:31 +0900

    jun taek

--- SST/src/org/sst/domain/StudyNote.java
+++ SST/src/org/sst/domain/StudyNote.java
@@ -9,7 +9,7 @@ public class StudyNote implements Serializable {
 	private String sn_contents; //노트 내용
 	private String sn_date; //노트 작성날짜
 	private String g_num; //어떤 그룹의 노트인지 알기위한 그룹고유번호 
-	private String gm_num; //어떤 그룹원이 작성했는지 그룹원고유번호
+	private String gm_num; //어떤 그룹원이 작성했는지 그룹원고유번호dfsdd
 	
 	public StudyNote() {}
 
