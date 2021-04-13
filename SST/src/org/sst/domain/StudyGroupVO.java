403527adfbd482e5827dc92323e8c584dd353b99
imhyyoo <imhyyoo@gmail.com>
2021-04-13 10:33:48 +0900

    StudyGroupVO 카테고리 멤버변수 추가

--- SST/src/org/sst/domain/StudyGroupVO.java
+++ SST/src/org/sst/domain/StudyGroupVO.java
@@ -1,24 +1,28 @@
 package org.sst.domain;
 
-public class StudyGroupVO {
+import java.io.Serializable;
+
+public class StudyGroupVO implements Serializable {
 	
 	private String g_num;
 	private String g_name;
 	private String g_contents;
 	private String g_secreat; //비밀번호를 할지말지
 	private String g_passwd; // 비밀번호
+	private String g_category;
 	
 	
 	public StudyGroupVO() {}
 
-	public StudyGroupVO(String g_num, String g_name, String g_contents, String g_secreat, String g_passwd) {
+	public StudyGroupVO(String g_num, String g_name, String g_contents, String g_secreat, String g_passwd,
+			String g_category) {
 		super();
 		this.g_num = g_num;
 		this.g_name = g_name;
 		this.g_contents = g_contents;
 		this.g_secreat = g_secreat;
 		this.g_passwd = g_passwd;
-		
+		this.g_category = g_category;
 	}
 
 	public String getG_num() {
@@ -61,6 +65,11 @@ public class StudyGroupVO {
 		this.g_passwd = g_passwd;
 	}
 
-	
+	public String getG_category() {
+		return g_category;
+	}
 
+	public void setG_category(String g_category) {
+		this.g_category = g_category;
+	}
 }
